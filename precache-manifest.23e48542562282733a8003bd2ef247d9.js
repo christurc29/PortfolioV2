188bd9944931678106f8f8c81d1e089e72ff51af
christurc29 <chris.turcios@protonmail.com>
2023-11-13 22:22:12 -0600

    Add files via upload

---
+++ precache-manifest.23e48542562282733a8003bd2ef247d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4e9bc6f714ca3bc5a8396d95ac076b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1e1119ec44d7cd86d35",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1f2397b66e41147105b6",
+    "url": "./static/css/main.f94b786c.chunk.css"
+  },
+  {
+    "revision": "c1e1119ec44d7cd86d35",
+    "url": "./static/js/2.b2c096af.chunk.js"
+  },
+  {
+    "revision": "3e9103a3e454762c847c57a8cd80d396",
+    "url": "./static/js/2.b2c096af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2397b66e41147105b6",
+    "url": "./static/js/main.6fd95101.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
